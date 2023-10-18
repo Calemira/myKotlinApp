d329c706340482cf3e6e80f7dd5f53fc2a0b605e
wercik0908@gmail.com <wercik0908@gmail.com>
2023-10-18 15:34:51 +0200

    Stworzenie 1 i 2 aktywnosci

---
+++ app/src/main/java/com/example/mykotlinapp/AboutMeActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mykotlinapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class AboutMeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aboutme)
+    }
+
+    fun startQuiz(view: View) {
+        val intent = Intent(this, QuizActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun returnToMain(view: View) {
+        finish()
+    }
+}
